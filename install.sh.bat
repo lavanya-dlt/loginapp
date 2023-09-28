213de11a178dd3498e238fbdc8e2c2923116f814
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-09-29 04:12:19 +0900

    Update install.sh.bat to remove redundant NPMs.

--- install.sh.bat
+++ install.sh.bat
@@ -1,12 +1,4 @@
 echo "Use sh install.sh.bat to run on Linux"
-npm install archiver
 npm install mustache
 npm install bcryptjs
 npm install sqlite3
-npm install @dqbd/tiktoken
-npm install natural
-npm install java
-npm install undici
-npm install htmlparser2
-npm install novel-segment
-npm install tinyld
